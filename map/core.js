a5f356024d92c2d337f01a7d1a93f9b685e3d6f3
Murph Grainger <murph.grainger@gmail.com>
2017-03-09 10:48:27 -0700

    finished reduce exercises

--- map/core.js
+++ map/core.js
@@ -1,22 +1,42 @@
-function multiplyBy10 (array) {
- // your code here
+array = ['hello', 'tomorrow', 'summer'];
+
+function multiplyBy10(array) {
+    let arrMultiply = array.map(function(num) {
+        return num * 10;
+    });
+    return arrMultiply;
 };
 
-function shiftRight (array) {
- // your code here
-};
+function shiftRight(array) {
+    let arrayRight = [];
+    arrayRight = array.map(function(num, index, array) {
+        if (index === 0) {
+            return array[array.length - 1];
+        } else {
+            return array[index - 1];
+        }
+    });
+    return arrayRight;
+}
 
-function onlyVowels (array) {
- // your code here
-};
+function onlyVowels(array) {
+    let newArray = [];
+    newArray = array.map(function(num, index, array) {
+        if () {
+            return array[index];
+        }
+    });
+}
 
-function doubleMatrix (array) {
- // your code here
+console.log(onlyVowels(array));
+
+function doubleMatrix(array) {
+    // your code here
 };
 
 module.exports = {
-  multiplyBy10: multiplyBy10,
-  shiftRight: shiftRight,
-  onlyVowels: onlyVowels,
-  doubleMatrix: doubleMatrix
+    multiplyBy10: multiplyBy10,
+    shiftRight: shiftRight,
+    onlyVowels: onlyVowels,
+    doubleMatrix: doubleMatrix
 };
--- reduce/core.js
+++ reduce/core.js
@@ -1,22 +1,40 @@
-function sum (array) {
-  // your code here
+function sum(array) {
+    return array.reduce((acc, val) => {
+        acc = acc + val;
+        return acc;
+    })
 };
 
-function productAll (array) {
-  // your code here
+function productAll(array) {
+    return array.reduce((acc, val) => {
+        acc *= val.reduce((prev, curr) => {
+            return prev * curr;
+        });
+        return acc;
+    }, 1)
 };
 
-function objectify (array) {
-  // your code here
-};
+function objectify(array) {
+    return array.reduce((acc, row) => {
+        acc[row[0]] = row[1];
+        return acc;
+    }, {});
+}
 
-function luckyNumbers (array) {
-  // your code here
+function luckyNumbers(array) {
+    let initial = 'Your lucky numbers are: '
+    return array.reduce((acc, num, index, nums) => {
+        if (index == nums.length - 1) {
+            return acc += (`and ${num}`);
+        } else {
+            return acc += (`${num}, `);
+        }
+    }, initial)
 };
 
 module.exports = {
-  sum: sum,
-  productAll: productAll,
-  objectify: objectify,
-  luckyNumbers: luckyNumbers
+    sum: sum,
+    productAll: productAll,
+    objectify: objectify,
+    luckyNumbers: luckyNumbers
 };
